e4c49b9f3747a91e6aa987f3a4c538dfa2860cb9
Brandon Keepers <brandon@opensoul.org>
2011-09-21 18:48:18 -0400

    Clear Qu.connection between tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'qu/backend/spec'
 
 RSpec.configure do |config|
   config.before do
+    Qu.connection = nil
     Qu.backend = mock('a backend', :reserve => nil, :failed => nil, :completed => nil,
       :register_worker => nil, :unregister_worker => nil)
   end
