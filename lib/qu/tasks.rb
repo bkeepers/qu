9dd308e270096ab1c9219a9a96c0ba1d67f75858
Brandon Keepers <brandon@opensoul.org>
2011-09-22 14:16:13 -0400

    Fix rake task

--- lib/qu/tasks.rb
+++ lib/qu/tasks.rb
@@ -1,10 +1,7 @@
 namespace :qu do
-
-  describe "Start a worker"
+  desc "Start a worker"
   task :work  => :environment do
     queues = (ENV['QUEUES'] || ENV['QUEUE'] || 'default').to_s.split(',')
-
     Qu::Worker.new(*queues).start
   end
-
 end
