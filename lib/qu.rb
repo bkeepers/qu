e45aba43dc90764cdcecfc80322c00084c89469c
Chris Moore <moorecp@gmail.com>
2011-10-10 14:12:11 -0400

    Only use railtie if Rails::Railtie is defined (Rails 3)

--- lib/qu.rb
+++ lib/qu.rb
@@ -35,4 +35,11 @@ Qu.configure do |c|
   c.logger.level = Logger::INFO
 end
 
-require 'qu/railtie' if defined?(Rails)
+if defined?(Rails)
+  if defined?(Rails::Railtie)
+    require 'qu/railtie'
+  else
+    Qu.logger = Rails.logger
+  end
+end
+
