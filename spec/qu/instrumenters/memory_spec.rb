80ec0154a8c76c71464d16f4e4664ca991f4add6
John Nunemaker <nunemaker@gmail.com>
2013-12-27 19:33:08 -0500

    Add noop and memory instrumenters.

---
+++ spec/qu/instrumenters/memory_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'qu/instrumenters/memory'
+
+describe Qu::Instrumenters::Memory do
+  describe "#initialize" do
+    it "sets events to empty array" do
+      instrumenter = described_class.new
+      instrumenter.events.should eq([])
+    end
+  end
+
+  describe "#instrument" do
+    it "adds to events" do
+      instrumenter = described_class.new
+      name         = 'user.signup'
+      payload      = {:email => 'john@doe.com'}
+      block_result = :yielded
+
+      result = instrumenter.instrument(name, payload) { block_result }
+      result.should eq(block_result)
+
+      event = described_class::Event.new(name, payload, block_result)
+      instrumenter.events.should eq([event])
+    end
+  end
+end
---
+++ spec/qu/instrumenters/noop_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'qu/instrumenters/noop'
+
+describe Qu::Instrumenters::Noop do
+  describe ".instrument" do
+    context "with name" do
+      it "yields block" do
+        yielded = false
+        described_class.instrument(:foo) { yielded = true }
+        yielded.should be_true
+      end
+    end
+
+    context "with name and payload" do
+      it "yields block" do
+        yielded = false
+        described_class.instrument(:foo, {:pay => :load}) { yielded = true }
+        yielded.should be_true
+      end
+    end
+  end
+end
