3d47311a1d5ac6ab2927633ef9faeff2a47cf2be
John Nunemaker <nunemaker@gmail.com>
2013-12-28 12:33:11 -0500

    No need for plural delegators.

--- lib/qu.rb
+++ lib/qu.rb
@@ -19,7 +19,7 @@ module Qu
   attr_accessor :backend, :failure, :logger, :graceful_shutdown, :instrumenter
 
   def_delegators :backend, :push, :pop, :size, :clear
-  def_delegators :instrumenter, :instrument
+  def_delegator :instrumenter, :instrument
 
   def backend
     @backend || raise("Qu backend not configured. Install one of the backend gems like qu-redis.")
