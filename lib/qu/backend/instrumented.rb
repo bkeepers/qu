f4d6d5f672ccf851f448110d8049486f5d42f98e
John Nunemaker <nunemaker@gmail.com>
2013-12-29 22:52:25 -0500

    Add instrumented backend.
    Going to centralize as much instrumentation as I can.

---
+++ lib/qu/backend/instrumented.rb
@@ -0,0 +1,59 @@
+require 'forwardable'
+
+module Qu
+  module Backend
+    # Internal: Backend that wraps all backends with instrumentation.
+    class Instrumented < Base
+      extend Forwardable
+
+      def_delegators :"Qu.instrumenter", :instrument
+      def_delegators :@backend, :connection, :connection=
+
+      def initialize(backend)
+        @backend = backend
+      end
+
+      def push(payload)
+        instrument("push.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:payload] = payload
+          @backend.push(payload)
+        }
+      end
+
+      def complete(payload)
+        instrument("complete.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:payload] = payload
+          @backend.complete(payload)
+        }
+      end
+
+      def abort(payload)
+        instrument("abort.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:payload] = payload
+          @backend.abort(payload)
+        }
+      end
+
+      def pop(queue_name = 'default')
+        instrument("pop.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:queue_name] = queue_name
+          @backend.pop(queue_name)
+        }
+      end
+
+      def size(queue_name = 'default')
+        instrument("size.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:queue_name] = queue_name
+          @backend.size(queue_name)
+        }
+      end
+
+      def clear(queue_name = 'default')
+        instrument("clear.#{InstrumentationNamespace}") { |ipayload|
+          ipayload[:queue_name] = queue_name
+          @backend.clear(queue_name)
+        }
+      end
+    end
+  end
+end
