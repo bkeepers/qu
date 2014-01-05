d2c4681d0f6a06e3de6dfd2f588cef48ea4fb3db
John Nunemaker <nunemaker@gmail.com>
2014-01-04 19:09:42 -0500

    Add instrumentation example.

---
+++ examples/instrumentation.rb
@@ -0,0 +1,28 @@
+# Example of how Qu works with graceful shutdown turned on.
+require 'socket'
+require_relative './example_setup'
+require 'qu/instrumentation/statsd'
+
+Thread.new do
+  socket = UDPSocket.new
+  socket.bind(nil, 6868)
+  loop do
+    puts socket.recvfrom(50)[0].chomp
+  end
+end
+
+# Config that matters to a user using qu
+# 1. setup instrumenter
+# 2. set client for subscriber
+Qu.configure do |config|
+  config.instrumenter = ActiveSupport::Notifications
+end
+Qu::Instrumentation::StatsdSubscriber.client = Statsd.new('localhost', 6868)
+
+class SimpleJob < Qu::Job
+end
+
+SimpleJob.create
+SimpleJob.create
+
+work_and_die
